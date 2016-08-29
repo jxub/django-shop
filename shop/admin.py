c45798abb5abac5206eb08314ba26d611e4d2544
Jakub Janarek <jjanarek@gmail.com>
2016-08-29 22:01:43 +0200

    Progressing with the bookshop. Yay!

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Cathegory, Product
+
+
+class CathegoryAdmin(admin.ModelAdmin):
+	list_display = ['name', 'slug']
+	prepopulated_fields = {'slug': ('name',)}
+admin.site.register(Cathegory, CathegoryAdmin)
+
+
+class ProductAdmin(admin.ModelAdmin):
+	list_display = ['name', 'slug', 'price', 'stock', 'available', 'created', 'updated']
+	list_filter = ['available', 'created', 'updated']
+	list_editable = ['price', 'stock', 'available']
+	prepopulated_fields = {'slug': ('name',)}
+admin.site.register(Product, ProductAdmin)
