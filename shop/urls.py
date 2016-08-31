b108a7b81121ec34b9ca9b6623940f0bdeb0a539
Jakub Janarek <jjanarek@gmail.com>
2016-08-31 14:55:17 +0200

    urls update

--- shop/urls.py
+++ shop/urls.py
@@ -4,6 +4,10 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.product_list, name='product_list'),
-    url(r'^(?P<category_slug>[-\w]+)/$', views.product_list, name='product_list_by_category')
-    url(r'^(?P<id>\d+)/(?P<slug>[-\w]+)/$')
+    url(r'^(?P<category_slug>[-\w]+)/$',
+    	views.product_list,
+    	name='product_list_by_category'),
+    url(r'^(?P<id>\d+)/(?P<slug>[-\w]+)/$',
+    	views.product.detail,
+    	name='product_detail'),
 ]
